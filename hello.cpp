6f213a10724d1da7a93e8694bc6658adf8b6f405
Aluno IFMG-Campus Bambuí <aluno@LAB01-DSKX>
2018-06-19 23:11:12 +0000

    hello world

---
+++ hello.cpp
@@ -0,0 +1,49 @@
+#include<systemc>
+#include<iostream>
+
+using namespace std;
+using namespace sc_core;
+
+SC_MODULE (hello_world) {
+
+	sc_in<bool> a, b;
+
+	sc_out<bool> c;
+
+
+	SC_CTOR (hello_world) {
+		SC_METHOD(executar);
+		sensitive << a << b;
+	}
+
+	void executar() {
+		if(a.read() && b.read()){
+			cout << "Hello World" << endl;
+			c.write(!c.read());
+		}
+	}
+
+};
+
+int sc_main(int argc, char **argv){
+	sc_signal<bool> entrada_a, entrada_b, saida_c;
+
+	hello_world hw("hello");
+
+	entrada_a.write(true);
+	entrada_b.write(true);
+
+	sc_start();
+
+	hw.a(entrada_a);
+	hw.b(entrada_b);
+	hw.c(saida_c);
+
+	cout << "" << saida_c.read() << endl;
+	return 0;
+}
+
+/* pra executar
+
+
+*/
